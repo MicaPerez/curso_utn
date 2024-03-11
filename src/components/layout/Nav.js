b449b419fdce103e0eae130a9034ba4d244cae88
MicaPerez <152228192+MicaPerez@users.noreply.github.com>
2024-03-11 18:38:57 -0300

    Add files via upload

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,14 +1,15 @@
-import { link } from "react-router.dom";
+import '../../styles/components/layout/Nav.css';
+import { NavLink } from "react-router-dom";
 
 const Nav = (props) => {
     return(
         <nav>
             <div>
                 <ul>
-                    <li><link to="/"> Home</link></li>
-                    <li><link to="nosotros"> Home</link></li>
-                    <li><link to="novedades"> Home</link></li>
-                    <li><link to="/contacto"> Home</link></li>
+                    <li>< NavLink to="/" className={({ isActive }) => isActive ? "activo" : undefined }> Home </NavLink></li>
+                    <li>< NavLink to="/nosotros" className={({ isActive }) => isActive ? "activo" : undefined }> Nosotros </NavLink></li>
+                    <li>< NavLink to="/novedades" className={({ isActive }) => isActive ? "activo" : undefined }> Novedades </NavLink></li>
+                    <li>< NavLink to="/contacto" className={({ isActive }) => isActive ? "activo" : undefined }> Contacto </NavLink></li>
                 </ul>
             </div>
         </nav>
